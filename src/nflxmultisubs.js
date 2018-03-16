dannvix/NflxMultiSubs

commit 9347c7dc891bf9326cae8b2a79107569cd5dad32

Refine settings names (top -> upper, btm -> lower)

src/nflxmultisubs.js
@@ -18,9 +18,8 @@ let gVideoRatio = (1080 / 1920);
 
 // FIXME: dedup default settings with background script
 const kDefaultSettings = {
-  centerLinePos: 0.5,
-  topBaselinePos: 0.15,
-  btmBaselinePos: 0.85,
+  upperBaselinePos: 0.15,
+  lowerBaselinePos: 0.85,
   primaryImageScale: 0.75,
   primaryImageOpacity: 0.85,
   primaryTextScale: 0.95,
@@ -224,16 +223,16 @@ class ImageSubtitle extends SubtitleBase {
 
   _render(lines, options) {
     const scale = options.secondaryImageScale;
-    const centerLine = this.extentHeight * options.centerLinePos;
-    const topBaseline = this.extentHeight * options.topBaselinePos;
-    const btmBaseline = this.extentHeight * options.btmBaselinePos;
+    const centerLine = this.extentHeight * 0.5;
+    const upperBaseline = this.extentHeight * options.upperBaselinePos;
+    const lowerBaseline = this.extentHeight * options.lowerBaselinePos;
     return lines.map(line => {
       const img = document.createElementNS('http://www.w3.org/2000/svg', 'image');
       this.zip.file(line.imageName).async('blob').then(blob => {
         const { left, top, width, height } = line;
         const [ newWidth, newHeight ] = [ width * scale, height * scale ];
         const newLeft = (left + 0.5 * (width - newWidth));
-        const newTop = (top <= centerLine) ? (topBaseline) : (btmBaseline);
+        const newTop = (top <= centerLine) ? (upperBaseline) : (lowerBaseline);
 
         const src = URL.createObjectURL(blob);
         img.setAttributeNS('http://www.w3.org/1999/xlink', 'href', src);
@@ -436,9 +435,9 @@ class PrimaryImageTransformer {
 
       // TODO: if there's no secondary subtitle, center the primary on baseline
       const options = gRenderOptions;
-      const centerLine = extentHeight * options.centerLinePos;
-      const topBaseline = extentHeight * options.topBaselinePos;
-      const btmBaseline = extentHeight * options.btmBaselinePos;
+      const centerLine = extentHeight * 0.5;
+      const upperBaseline = extentHeight * options.upperBaselinePos;
+      const lowerBaseline = extentHeight * options.lowerBaselinePos;
       const scale = options.primaryImageScale;
       const opacity = options.primaryImageOpacity;
 
@@ -459,7 +458,7 @@ class PrimaryImageTransformer {
 
         const [ newWidth, newHeight ] = [ width * scale, height * scale ];
         const newLeft = (left + 0.5 * (width - newWidth));
-        const newTop = (top <= centerLine) ? (topBaseline - newHeight) : (btmBaseline - newHeight);
+        const newTop = (top <= centerLine) ? (upperBaseline - newHeight) : (lowerBaseline - newHeight);
         img.setAttributeNS(null, 'width', newWidth);
         img.setAttributeNS(null, 'height', newHeight);
         img.setAttributeNS(null, 'x', newLeft);

src/background.js
@@ -13,9 +13,8 @@ chrome.webRequest.onBeforeRequest.addListener(details => {
 
 
 const kDefaultSettings = {
-  centerLinePos: 0.5,
-  topBaselinePos: 0.15,
-  btmBaselinePos: 0.85,
+  upperBaselinePos: 0.15,
+  lowerBaselinePos: 0.85,
   primaryImageScale: 0.75,
   primaryImageOpacity: 0.85,
   primaryTextScale: 0.95,
@@ -29,9 +28,19 @@ const kDefaultSettings = {
 
 let gSettings = Object.assign({}, kDefaultSettings);
 
+// return true if valid; otherwise return false
+function validateSettings(settings) {
+  const keys = Object.keys(kDefaultSettings);
+  return keys.every(key => (key in settings));
+}
+
+
 chrome.storage.local.get(['settings'], (result) => {
-  console.dir('Loaded: settings=', result.settings);
-  gSettings = result.settings || gSettings;
+  console.log('Loaded: settings=', result.settings);
+  if (result.settings && validateSettings(result.settings))
+    gSettings = result.settings;
+  else
+    saveSettings();
 });
 
 function saveSettings() {
@@ -96,11 +105,6 @@ chrome.runtime.onConnectExternal.addListener(port => {
 
 // -----------------------------------------------------------------------------
 
-// return true if valid; otherwise return false
-function validateSettings(settings) {
-  return true; // TODO
-}
-
 
 chrome.runtime.onConnect.addListener(port => {
   const portName = port.name;
@@ -110,7 +114,7 @@ chrome.runtime.onConnect.addListener(port => {
     port.postMessage({ settings: gSettings });
 
     port.onMessage.addListener(msg => {
-      console.dir('Received: settings=', msg.settings);
+      console.log('Received: settings=', msg.settings);
       if (!msg.settings) {
         gSettings = Object.assign({}, kDefaultSettings);
         port.postMessage({ settings: gSettings });

src/settings.js
@@ -10,16 +10,16 @@ port.onMessage.addListener((msg) => {
 
 const layoutPresets = [
   { // compact
-    topBaselinePos: 0.20,
-    btmBaselinePos: 0.80,
+    upperBaselinePos: 0.20,
+    lowerBaselinePos: 0.80,
   },
   { // moderate (default)
-    topBaselinePos: 0.15,
-    btmBaselinePos: 0.85,
+    upperBaselinePos: 0.15,
+    lowerBaselinePos: 0.85,
   },
   { // ease
-    topBaselinePos: 0.10,
-    btmBaselinePos: 0.90,
+    upperBaselinePos: 0.10,
+    lowerBaselinePos: 0.90,
   },
 ];
 
@@ -89,7 +89,7 @@ function renderActiveSettings() {
   let elem;
 
   // layout
-  const layoutId = layoutPresets.findIndex(k => (k.btmBaselinePos === settings.btmBaselinePos));
+  const layoutId = layoutPresets.findIndex(k => (k.lowerBaselinePos === settings.lowerBaselinePos));
   if (layoutId !== -1) {
     elem = document.querySelector(`.settings-layout > div[data-id="${layoutId}"]`);
     elem && elem.classList.add('active');

commit 8206e1ff539f09043bdbb6b22c8f91dfb51bc322

Fix text-based secondary subtitles layout error
Bug introduced in with commit 9347c7dc.

src/nflxmultisubs.js
@@ -158,7 +158,7 @@ class TextSubtitle extends SubtitleBase {
     text.setAttributeNS(null, 'stroke', 'black');
     text.setAttributeNS(null, 'stroke-width', `${1.0 * options.secondaryTextStroke}px`);
     text.setAttributeNS(null, 'x', this.extentWidth * 0.5);
-    text.setAttributeNS(null, 'y', this.extentHeight * (options.btmBaselinePos + 0.01));
+    text.setAttributeNS(null, 'y', this.extentHeight * (options.lowerBaselinePos + 0.01));
     text.setAttributeNS(null, 'opacity', options.secondaryTextOpacity);
     text.style.fontSize = `${fontSize * options.secondaryTextScale}px`;
     text.style.fontFamily = 'Arial, Helvetica';
@@ -521,10 +521,10 @@ class PrimaryTextTransformer {
     const rect = divElem.getBoundingClientRect();
     const [ extentWidth, extentHeight ] = [ rect.width, rect.height ];
 
-    const btmBaseline = extentHeight * options.btmBaselinePos;
+    const lowerBaseline = extentHeight * options.lowerBaselinePos;
     const { left, top, width, height } = container.getBoundingClientRect();
     const newLeft = ((extentWidth * 0.5) - (width * 0.5));
-    let newTop = (btmBaseline - height);
+    let newTop = (lowerBaseline - height);
 
     // FIXME: dirty transform & magic offets
     // we out run the official player, so the primary text-based subtitles